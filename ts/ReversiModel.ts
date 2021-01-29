5c504bbcfe320ac4677448ec7b55086689ecf0a6
l3miage-agossoud <romeo.agossou50@gmail.com>
2021-01-29 01:24:50 +0100

    le_nouveau_romeo

--- ts/ReversiModel.ts
+++ ts/ReversiModel.ts
@@ -5,26 +5,96 @@ import {Board, Turn, ReversiModelInterface, C, TileCoords, Board_RO, GameState, 
 export class ReversiModel implements ReversiModelInterface {
     protected board: Board;
     protected currentTurn: Turn;
+
+    protected game: GameState;
+
     protected gameStateSubj: BehaviorSubject<GameState>;
+
     
     public readonly gameStateObs: Observable<GameState>;
+    
 
     constructor() {
+        this.gameStateSubj = new BehaviorSubject<GameState>(this.game);
+        this.gameStateObs = this.gameStateSubj.asObservable();
         this.initBoard();
     }
 
+
     initBoard(): void {
+        this.currentTurn = this.game.turn='Player1';
+        this.board = new Array(8).fill(0).map( l => new Array<C>(8).fill(C.Empty) ) as Board;
+        this.board[3][3] = this.board[4][4] = this.game.turn = 'Player1';
+        this.board[4][3] = this.board[3][4] = this.game.turn = 'Player2';;
     }
 
     PionsTakenIfPlayAt(x: number, y: number): PlayImpact {
         // Parcourir les 8 directions pour accumuler les coordonnées de pions prenables
-        return [];
-    }
+
+        const FctAccuDir = (dx :number, dy: number): PlayImpact => {
+                // renvoie les coordonnées des pions pris dans la direction dx, dy
+
+                if (this.board[x][y] === 'Empty') {
+                    const otherTile = this.currentTurn === (this.game.turn = 'Player1') ? (this.game.turn = 'Player2') : (this.game.turn = 'Player1');
+                    const tilesToFlip: PlayImpact = [];
+
+                const tab : TileCoords[] = [];
+                let px=x;
+                let py=y;
+                let c: C;
+
+                do{
+                      px +=dx;
+                      py +=dy;
+                      //c= this.board[px][py];
+                      c= (px >= 0 && px < this.board.length &&  py >=0 && py < this.board[0].length) ?this.board[px][py] : 'Empty';
+                      //c= this.board[px] ? this.board[px] : C.Empty;
+                      
+                      tab.push({x= px, y= py});
+
+                }while(c === otherTile);
+                    tab.pop();
+                    return c === this.currentTurn ? tab : [];
+                }
+                    
+                
+                
+    
+        }
+    return [];
+}
+
+
 
     play(i: number, j: number): void {
         // Vérifier que le coup est valide.
         // Si c'est le cas, après avoir jouer le coup, on passe à l'autre joueur.
         // Si l'autre joueur ne peut jouer nul part, on redonne la main au joueur initial. 
+        const L = this.PionsTakenIfPlayAt(i, j);
+        if (L.length > 0) {
+            // à compléter
+            this.board[i][j] = this.currentTurn; // Je place mon pion
+            L.forEach( ({x, y}) => this.board[x][y] = this.currentTurn );
+            // Je change de joueur
+            this.currentTurn = this.currentTurn === (this.game.turn = 'Player1') ? (this.game.turn = 'Player2') : (this.game.turn = 'Player1');
+            if (this.skipTurn()) {
+                // L'adversaire ne peut pas jouer, c'est encore à moi
+                this.currentTurn = this.currentTurn === (this.game.turn = 'Player1') ? (this.game.turn = 'Player2') : (this.game.turn = 'Player1');
+            }
+            this.gameStateSubj.next(this.game);
+        }
+    }
+
+
+    skipTurn(): boolean {
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (this.PionsTakenIfPlayAt(i, j).length > 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
--- ts/ReversiPresenter.ts
+++ ts/ReversiPresenter.ts
@@ -1,4 +1,4 @@
-import {GameState, ReversiModelInterface} from './ReversiDefinitions';
+import { GameState, ReversiModelInterface, Turn, Board } from './ReversiDefinitions';
 
 const reversiView = `
     <table class="reversi">
@@ -8,6 +8,7 @@ const reversiView = `
 
 export class ReversiPresenter {
     tds: HTMLElement[][] = [];
+    
 
     constructor(private root: HTMLElement, private model: ReversiModelInterface) {
         this.initBoard();
@@ -18,6 +19,18 @@ export class ReversiPresenter {
         // Les cases (balises td) contenant un pion du joueur 1 ont la classe CSS Player1 (voir le CSS)
         // Les cases (balises td) contenant un pion du joueur 2 ont la classe CSS Player2 (voir le CSS)
         // Les cases sur lesquelles le joueur courant peut poser un pion ont la classe CSS canPlayHere
+        this.tds.forEach((L,i)=>L.forEach((td, j)=>{
+            const c= board[i][j];
+            td.setAttribute("class",'');
+            td.classList.add(turn);
+             const canplay = this.model.PionsTakenIfPlayAt(i,j).length>0;
+             if(canplay){
+                 td.onclick = () => this.model.play(i,j);
+                 td.classList.add('canPlayHere');
+             }else{
+                 td.onclick=null;
+             }
+        }))
     }
 
     private initBoard() {
@@ -25,5 +38,23 @@ export class ReversiPresenter {
         // Remplir le tableau avec 8x8 cases contenant chacune une balise div
         // Utiliser la fonction document.createElement
         // Stockez les balises td dans l'attribut tds de l'objet => ça vous facilitera la vie plus tard
+
+        this.root.innerHTML = reversiView;
+        const tbody = this.root.querySelector('tbody') as HTMLElement;
+        const board = this.model.gameStateObs;
+        
+        let str =' ';
+        board.forEach( L => {
+            str +='<tr>';
+            L.board.forEach(c => {
+                str +='<td><div></div></td>'
+            })
+            str +='</tr>'
+        })
+        tbody.innerHTML = str;
+
+        this.tds = [...tbody.querySelectorAll('tr')].map(tr => [...tr.querySelectorAll('td')]);
+        console.log(this.tds);
+        //this.updatePresentation(board, turn );
     }
 }
